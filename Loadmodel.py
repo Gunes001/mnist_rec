f0b88dac5fcdf0a37b6ee1d4009b7e179e353d01
Gunes001 <John.M.Chen@outlook.com>
2025-01-02 12:14:52 -0500

    Bugfix for loadmodel.py

--- Loadmodel.py
+++ Loadmodel.py
@@ -3,8 +3,12 @@ import torch
 import torch.nn as nn
 import pandas as pd
 
+
+# 检查是否有可用的 MPS 设备
+device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+
 # 直接加载整个模型，并设置 weights_only=False
-model = torch.load("model.pth",weights_only=False)
+model = torch.load("model.pth",weights_only=False).to(device)
 model.eval()
 
 # 用Pandas读取MNIST数据abs
@@ -12,8 +16,8 @@ raw_data = pd.read_csv("mnist_test.csv")
 raw_data_features = raw_data.iloc[:,1:].values
 raw_data_labels = raw_data.iloc[:,0].values
 
-test_features = torch.tensor(raw_data_features[200:2000]).to(torch.float)
-test_labels = torch.tensor(raw_data_labels[200:2000]).to(torch.long)
+test_features = torch.tensor(raw_data_features[200:2000]).to(torch.float).to(device)
+test_labels = torch.tensor(raw_data_labels[200:2000]).to(torch.long).to(device)
 
 #预测
 with torch.no_grad():
